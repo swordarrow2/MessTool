ac855d3057fb9a27b038313c060bd55de1287a8c
swordarrow2 <2856986197@qq.com>
2024-08-29 19:21:00 +0800

    工具类

--- app/src/main/java/com/meng/tools/app/SystemTools.java
+++ app/src/main/java/com/meng/tools/app/SystemTools.java
@@ -16,6 +16,22 @@ import static android.content.Context.*;
  */
 
 public class SystemTools {
+
+    public static String getDateAndTime() {
+        Calendar c = Calendar.getInstance();
+
+        String year = String.valueOf(c.get(Calendar.YEAR));
+        String month = String.valueOf(c.get(Calendar.MONTH) + 1);
+        String day = String.valueOf(c.get(Calendar.DAY_OF_MONTH));
+        String hour = String.valueOf(c.get(Calendar.HOUR_OF_DAY));
+        String mins = String.valueOf(c.get(Calendar.MINUTE));
+
+
+        StringBuilder builder = new StringBuilder();
+        builder.append(year).append("-").append(month).append("-").append(day).append(" ").append(hour).append(":").append(mins);
+        return builder.toString();
+    }
+
     public static boolean isUsingWifi() {
         return ((ConnectivityManager) ApplicationHolder.getActivity().getSystemService(Context.CONNECTIVITY_SERVICE)).getNetworkInfo(ConnectivityManager.TYPE_WIFI).isConnected();
     }

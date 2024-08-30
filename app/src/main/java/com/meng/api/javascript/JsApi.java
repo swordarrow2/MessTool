2b2dfce90add7653da4432b91cce9800a079d644
swordarrow2 <2856986197@qq.com>
2024-08-30 20:46:57 +0800

    重命名

--- app/src/main/java/com/meng/api/javascript/JsApi.java
+++ app/src/main/java/com/meng/api/javascript/JsApi.java
@@ -1,16 +1,16 @@
-package com.meng.api.jsBridge;
+package com.meng.api.javascript;
 
 import android.webkit.*;
 
 import com.meng.messtool.*;
 
-public class JsInterfaceObject {
+public class JsApi {
 
     /*
      *@author 清梦
      *@date 2024-08-28 19:28:37
      */
-    public static final String TAG = "JsInterfaceObject";
+    public static final String TAG = "JsApi";
     private MainActivity mainActivity;
 
     public void setMainActivity(MainActivity mainActivity) {
